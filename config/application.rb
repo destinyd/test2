2ef79f0efa474d768fe4eadbd4d97b76cb4fb8d7
destinyd <destinyd.war@gmail.com>
2011-09-05 09:11:46 +0800

    modify for 31

--- config/application.rb
+++ config/application.rb
@@ -39,6 +39,8 @@ module Zhekou
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+
+    # Enable the asset pipeline
     config.assets.enabled = true
   end
 end
