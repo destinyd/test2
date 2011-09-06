d896ae2db495bb72b8cdbb6cc0b9da59545a5ef7
destinyd <destinyd.war@gmail.com>
2011-09-06 15:29:36 +0800

    gitignore

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,39 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+function show_and_hide(show,hide){
+  $(show).show();
+  $(hide).hide();
+  return false;
+}
+function ajaxForm(formdom,action){
+      if(action == "get")
+        formdom.submit(function () {
+          $.get(this.action, $(this).serialize(), null, 'script');
+          return false;
+        });
+      else
+        formdom.submit(function () {
+          $.post(this.action, $(this).serialize(), null, 'script');
+          return false;
+        });
+
+}
+    $(function () {
+
+      $('#goods th a, .pagination a').live('click',
+        function () {
+          $.getScript(this.href);
+          return false;
+        }
+      );
+      // Search form.
+      $('#goods_search').submit(function () {
+        $.get(this.action, $(this).serialize(), null, 'script');
+        return false;
+      });
+
+      ajaxForm($('#new_comment'));
+
+    })
+
