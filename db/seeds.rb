7ff88d93c6c7e8acc63927cbb523f11fcbabe965
destinyd <destinyd_war@163.com>
2011-11-10 10:12:18 +0800

    cdata

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,19 +18,18 @@ TuanApi.create [
   :docfind => "//urlset/url",
   :suite  => "d = d.children[1].first;d.each{|a| n[a.name] = a.content};p.price = n['price'];p.type_id = \"团购价\";p.finish_at = n['endTime'].to_i;p.address = n['city'];p.title=n['title'];o = p.outlinks.new;o.url = n['loc'];p.uploads.new(:image_file_name => n['image']);",
   :tuan_urls=> [
-    TuanUrl.new( {      :name => "F团柳州", :url => "http://newapi.ftuan.com/api/v2.aspx?city=liuzhou", :enable => true}),
+    TuanUrl.new( {      :name => "24卷柳州", :url => "http://static.24quan.com/cms/liuzhou_bus.php", :enable => true}),
     TuanUrl.new( {      :name => "拉手团柳州", :url => "http://open.client.lashou.com/api/detail/city/427", :enable => true}),
     TuanUrl.new( {      :name => "信团柳州", :url => "http://www.cn403.com/api/hao123.php", :enable => true}),
     TuanUrl.new( {      :name => "糯米团柳州", :url => "http://www.nuomi.com/api/dailydeal?version=v1&city=liuzhou", :enable => true}),
-    TuanUrl.new( {      :name => "团宝柳州", :url => "http://p5.groupon.cn/xml/city/cityproduct/?cityid=236", :enable => true}),
     TuanUrl.new( {      :name => "美团柳州", :url => "http://www.meituan.com/api/v1/liuzhou/deals", :enable => true}),
     TuanUrl.new( {      :name => "米奇", :url => "http://www.miqi.cn/xml/apis/lab.api.hao123.xml", :enable => true}),
     TuanUrl.new( {      :name => "聚美优品", :url => "http://www.jumei.com/api/dealsv3.php", :enable => true}),
     TuanUrl.new( {      :name => "爱帮团", :url => "http://tuan.aibang.com/api/digest", :enable => true}),
-    TuanUrl.new( {      :name => "高朋", :url => "http://tuan1212.com/gaopeng.php", :enable => true}),
-    TuanUrl.new( {      :name => "趣团", :url => "http://www.quwan.com/tuan/api/hao123.php", :enable => true}),
-    TuanUrl.new( {      :name => "24卷柳州", :url => "http://static.24quan.com/cms/liuzhou_bus.php", :enable => true})
-
+#    TuanUrl.new( {      :name => "高朋", :url => "http://tuan1212.com/gaopeng.php", :enable => true}),
+#    TuanUrl.new( {      :name => "趣团", :url => "http://www.quwan.com/tuan/api/hao123.php", :enable => true}),
+    TuanUrl.new( {      :name => "团宝柳州", :url => "http://p5.groupon.cn/xml/city/cityproduct/?cityid=236", :enable => true}),
+    TuanUrl.new( {      :name => "F团柳州", :url => "http://newapi.ftuan.com/api/v2.aspx?city=liuzhou", :enable => true}),
   ]
   },
 ]
