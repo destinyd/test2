84547b436a27aa8c29ce5512801b10eae801b087
destinyd <destinyd_war@163.com>
2012-01-10 10:37:36 +0800

    good show

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,3 +59,17 @@ function good_bound(){
   }
   return false;
 }
+
+function get_attrs(id){
+  $.getJSON('/goods/'+id+'/attrs', function(data) {
+    var items = [];
+    $.each(data, function(key, val) {
+        items.push('<li id="attr_' + key + '" title="查看更多'+key+'信息"><a href="/goods/' + id + '/attrs/' + key +'" data-remote="true">' + key +'</a>:' + val + '</li>');
+    });
+    $('#attrs').html(items.join(''));
+    $('#a_attrs').attr('onclick','');
+    $('#a_attrs').click(function(){
+      show_and_hide('.product_attrs','.goods_div')
+    });
+  });
+}
