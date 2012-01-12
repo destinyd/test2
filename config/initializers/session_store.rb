a86eac3abde766d5a16674f08c0fe25c79e9b313
destinyd <destinyd_war@163.com>
2012-01-12 17:04:15 +0000

    setting

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-#Zhekou::Application.config.session_store :cookie_store, :key => '_zhekou_session'
-Zhekou::Application.config.session_store :redis_session_store
+Zhekou::Application.config.session_store :cookie_store, :key => '_zhekou_session'
+#Zhekou::Application.config.session_store :redis_store
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
