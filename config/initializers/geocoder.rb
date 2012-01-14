2aea22d217157c8b3c44cbe8aca4b71eae4318b4
destinyd <destinyd_war@163.com>
2012-01-14 14:00:54 -0500

    home

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,13 @@
-Geocoder::Configuration.timeout = 5
+Geocoder::Configuration.timeout = 10
+Geocoder::Configuration.lookup = :google
+#Geocoder::Configuration.api_key = "ABQIAAAAUFVnNhzuomcHcRz3oniI6BQemdLajNn3kdAkc3yK93SdhxAJ7hTUzYXT_7TYC3szzx6tDzkp2NwXKA"
+#Geocoder::Configuration.api_key = "ABQIAAAAUFVnNhzuomcHcRz3oniI6BRTtBxSPqWhz0spOPxzRbxciF1_VBRqI4tPfp-UXC0U5ygc89jD6-IA7g"
+# use HTTPS for geocoding service connections:
+#Geocoder::Configuration.use_https = true
 Geocoder::Configuration.language = :"zh-CN"
+#Geocoder::Configuration.language = :en
+#Geocoder::Configuration.http_proxy  = "127.4.4.1"
+# use HTTPS for geocoding service connections:
+#Geocoder::Configuration.use_https = true
 #Geocoder::Configuration.cache = Redis.new
-Geocoder::Configuration.cache_prefix = "zhekou"
+#Geocoder::Configuration.cache_prefix = "zhekou"
