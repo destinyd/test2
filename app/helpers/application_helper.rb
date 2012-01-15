062987360622ca8c816ab875504457c62efa71d6
destinyd <destinyd_war@163.com>
2012-01-15 01:32:40 -0500

    geo

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def initialize(a,b,c)
+    super(a,b,c)
+    session[:loc] = request.location unless session[:loc]
+    @loc = session[:loc]
+    session[:geo_result] = Geocoder.search(@loc.city).first if session[:geo_result].nil? and @loc
+    @geo_result = session[:geo_result]
+  end
+
   def sortable(column, title = nil)  
     title ||= column.titleize  
     css_class = (column == sort_column) ? "current #{sort_direction}" : nil  
@@ -10,8 +18,8 @@ module ApplicationHelper
     class_constantize ||=  controller_name.singularize.capitalize.constantize
     diy_action ||= action_name
     t('titles.'+diy_action,:model => class_constantize.model_name.human)
-#    output  +=  model.model_name.human
-#    elsif action_name == 'new'
+    #    output  +=  model.model_name.human
+    #    elsif action_name == 'new'
   end
 
   def could_bread?
