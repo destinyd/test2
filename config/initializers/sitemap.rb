44cbb256c915267e9d75688a4abdc5c4a3f4f198
destinyd <destinyd_war@163.com>
2012-02-03 04:38:17 -0500

    sitemap

--- config/initializers/sitemap.rb
+++ config/initializers/sitemap.rb
@@ -1,28 +1,28 @@
 DynamicSitemaps::Sitemap.draw do
   
   # default per_page is 50.000 which is the specified maximum.
-  per_page 10
+  per_page 500
 
-  url root_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 1
-  new_page!
+  url root_url, :last_mod => Time.now.beginning_of_day, :change_freq => 'daily', :priority => 0.8
+  #new_page!
 
-  url costs_prices_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 0.8
-  url cheapest_prices_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 0.8
-  url groupbuy_prices_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 0.8
-  url just_started_prices_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 0.8
-  url nearly_finish_prices_url, :last_mod => DateTime.now, :change_freq => 'daily', :priority => 0.8
+  url costs_prices_url, :last_mod => DateTime.now.beginning_of_day, :change_freq => 'daily', :priority => 0.6
+  url cheapest_prices_url, :last_mod => DateTime.now.beginning_of_day, :change_freq => 'daily', :priority => 0.6
+  url groupbuy_prices_url, :last_mod => DateTime.now.beginning_of_day, :change_freq => 'daily', :priority => 0.6
+  url just_started_prices_url, :last_mod => DateTime.now.beginning_of_day, :change_freq => 'daily', :priority => 0.6
+  url nearly_finish_prices_url, :last_mod => DateTime.now.beginning_of_day, :change_freq => 'daily', :priority => 0.6
 
-  new_page!
+  #new_page!
   
   #Price.all.each do |price|
     #url price_url(price), :last_mod => price.updated_at, :change_freq => 'monthly', :priority => 0.7
   #end
   #new_page!
   
-  autogenerate  :prices,# :goods,
-                :last_mod => :updated_at,
-                :change_freq => 'monthly',
-                :priority => 0.5
+  #autogenerate  :prices,# :goods,
+                #:last_mod => :updated_at,
+                #:change_freq => 'monthly',
+                #:priority => 0.5
   #new_page!
   
 end
