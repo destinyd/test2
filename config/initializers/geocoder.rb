b3c8a1ae6cfb24a7873fc9b683504bf9b1e78abb
destinyd <destinyd_war@163.com>
2012-02-08 12:24:18 -0500

    timeout

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,4 +1,4 @@
-Geocoder::Configuration.timeout = 10
+Geocoder::Configuration.timeout = 5
 Geocoder::Configuration.lookup = :google
 #Geocoder::Configuration.api_key = "ABQIAAAAUFVnNhzuomcHcRz3oniI6BQemdLajNn3kdAkc3yK93SdhxAJ7hTUzYXT_7TYC3szzx6tDzkp2NwXKA"
 #Geocoder::Configuration.api_key = "ABQIAAAAUFVnNhzuomcHcRz3oniI6BRTtBxSPqWhz0spOPxzRbxciF1_VBRqI4tPfp-UXC0U5ygc89jD6-IA7g"
